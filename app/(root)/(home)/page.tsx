09408596915b3dfdb13f40f4c978c67b656b9da7
Ankush Shah <ankushsahvlogs@gmail.com>
2025-01-24 03:22:04 +0530

    hey

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal md:mb-8 lg:mb-0 md:m-8 lg:mt-0 lg:m-0">
-            Built with ❤️ by Haven Futures
+            Built with 💚 by Haven Futures
           </h2>
           <CurrentTime /> {/* Use the CurrentTime component here */}
         </div>
